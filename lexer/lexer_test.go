4104f653c2070aba89a561f159616ecb72b18cc3
alexfjw <alexfongg@gmail.com>
2020-01-14 14:51:42 +0800

    add more one char tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -14,6 +14,8 @@ let add = fn(x, y) {
 };
 
 let result = add(five,ten);
+!-/*5;
+5 < 10 > 5;
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -55,6 +57,18 @@ let result = add(five,ten);
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
@@ -63,7 +77,12 @@ let result = add(five,ten);
 	for i, tt := range tests {
 		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] tokentype wrong. expected_type=%q, val=%q, got_type=%q, val=%q",
+				i,
+				tt.expectedType,
+				tt.expectedLiteral,
+				tok.Type,
+				tok.Literal)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
--- token/token.go
+++ token/token.go
@@ -9,8 +9,15 @@ const (
 	INT   = "INT"
 
 	// operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// delimiters
 	COMMA     = ","

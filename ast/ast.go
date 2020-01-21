fc2cc22e64022358ea05c415c952bb0c951043da
alexfjw <alexfongg@gmail.com>
2020-01-21 16:46:24 +0800

    add test for precedence

--- ast/ast.go
+++ ast/ast.go
@@ -150,6 +150,7 @@ func (oe *InfixExpression) expressionNode()      {}
 func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
 func (oe *InfixExpression) String() string {
 	var out bytes.Buffer
+	out.WriteString("(")
 	out.WriteString(oe.Left.String())
 	out.WriteString(" " + oe.Operator + " ")
 	out.WriteString(oe.Right.String())
